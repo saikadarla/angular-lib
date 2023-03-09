e5afdccc385293b607aa46881d98356254aa40cc
saikadarla <89072491+saikadarla@users.noreply.github.com>
2023-03-09 18:56:49 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RadioButtonsComponent } from './radio-buttons/radio-buttons.component';
+
 
 const routes: Routes = [
-  { path: 'radio', component: RadioButtonsComponent }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app-radio-buttons';
+export class AppComponent implements OnInit{
+  title = 'Store management.....';
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _routerNavigate: Router
+    ) {
+
+  }
+
+  ngOnInit(): void{
+    
+    let title = 'hello Title field';
+    console.log('this.route: ', this._route);
+  }
+
+  onClick(): void{
+    console.log('called......');
+    this._routerNavigate.navigate(['./about'])
+  }
 }
